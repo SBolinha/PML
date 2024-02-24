f34f1cd20b5d11f3033b3fba8ab39673a57e76f6
SBolinha <157318106+SBolinha@users.noreply.github.com>
2024-02-24 14:50:39 +0100

    Corrected for <![CTEXT[]]> and <?xml namespace=".."> type of elements

--- xml2pml.py
+++ xml2pml.py
@@ -4,72 +4,82 @@ def xml2pml(xml, otll=0, tab='    ', tag='§.', att='@.', KeepCR=True):
 #   tag = '§.' # default element tag indicator
 #   att = '@.' # default attribute tag indicator
 #   KeepCR = True # by default transfer <CR> from the source XML
-    itll = 0 # inner tag level
-    atln = -1 # attribute length
+    atln = -1 # attribute tag length
     tlln = 0 # tag label length
-    nclt = True # non-closing tag
-    squot = False
-    dquot = False
+    squot = False # inside single quoted  content
+    dquot = False # inside double quoted content
+    intag = False # inside tag
+    ttype = '' # tag content type
+    pl = '' # previous character
+    l = '' # the current character being processed
 
-    pml = tab*otll          
+    pml = ''
     for l in xml:
-        if l == '>':
-            itll = 0
-            atln = -1
-            if nclt:
-                otll += 1
-            else:
-                otll -= 1
-            nclt = True
-        elif l == '/':
-            if tlln > 0:
-                otll -=1 # a self-closing tag, compensate for outer tag level increment later
-            else: # a closing tag
-                tlln = 1 # just a default value
-                nclt = False
-        elif l == '<':
-            itll = 1
+        if l == '<':
+            intag = True
+            ttype = '' # reset tag type
             tlln = 0
-        else:
-            if itll>0:
-                if nclt:
-                    if l == ' ' and not(squot or dquot): # remove the <space> in the non-attribute part of the tag, detection of attributes is possible
-                        atln=0
-                        itll = 1 # reset to default in tag level
-                        l = ''
-                    elif l == '\n': # remove the <space> or the <cr> in the tag, detection of attributes is possible
-                        atln=0
-                        itll = 1 # reset to default in tag level
-                        l = ''
-                    elif l == '"' and not(squot): # double quote level can only be actuated if no single quote level is active
-                        dquot = not(dquot)
-                    elif l == "'" and not(dquot): # sing;e quote level can only be actuated if no double quote level is active
+            atln = -1
+            pml += '\n' + tab * otll
+        elif l == '>':
+            intag = False
+            # Handle the indentation and <CR> after the tag
+            if ttype == '/':
+                otll = max(otll-1, 0) # hand brake procedure to avoid excessive outdentation
+                pml += '\n' + tab * otll
+            elif pl == '/': # self-closing tag
+                pml += '\n' + tab * otll
+            elif ttype in ['?', '!']: # treat as self-closing tag
+                pml += '\n' + tab * otll
+            else:
+                otll +=1
+                pml += '\n' + tab * otll
+        else: # implicitly: the < and > symbols are not sent to output, the rest is passed through
+            if intag:
+                if tlln == 0: # first character of the element tag
+                    if l in ['/', '?', '!']:
+                        ttype = l
+                    else:
+                        ttype = '<' # regular opening tag
+                        pml += tag # add the element tag identifier to allow reverse engineering
+                    tlln = 1
+
+                if ttype == '/':
+                    pass
+                elif l == '/' and not(squot) and not(dquot) and ttype=='<': #  do not process the slash
+                    pass
+                elif l == '\n': # <CR> in elements will be removed
+                    pass
+                elif l == ' ' and not(squot) and not(dquot) and ttype=='<': # potential for attributes
+                    l = '' # remove spaces
+                    atln = 0
+                elif atln == 0 and ttype=='<':
+                    pml += '\n' + tab * (otll + 1)
+                    atln += 1
+                    pml += att + l                    
+                    if l=="'" and not(dquot):
                         squot = not(squot)
-                    else: # tag or attribute character detected
-                        if atln<0: # attribute tag is not yet possible, must be a tag
-                            if tlln == 0:
-                                pml += '\n' + tab*otll + tag            
-                            tlln += 1
-                        elif atln==0: # first character of the attribute tag, following <space> or <crlf>
-                            itll +=1
-                            atln += 1
-                            pml += '\n' + tab*(otll + itll - 1) + att          
-                        else:
-                            atln +=1
-                    pml += l          
-                else:
-                    pass # nothing to be done for a closing tag
-            else: # outside an element
-                if tlln>0: # coming just out of an element
-                    if nclt:
-                        pml += '\n' + tab*otll         
-                    tlln = 0
-                if l == '\n':
-                    pml += '\n' + tab*otll
+                    elif l=='"' and not(squot):
+                        dquot = not(dquot)
+                elif atln > 0 and ttype=='<':
+                    atln += 1
+                    pml += l
+                    if l=="'" and not(dquot):
+                        squot = not(squot)
+                    elif l=='"' and not(squot):
+                        dquot = not(dquot)
+                else: # add the to the element tag label
+                    tlln += 1
+                    pml += l
+            else: # just content, but may depend on tag type
+                if l == '\n': # add left tabulation
+                    pml += '\n' + tab * otll
                 else:
                     pml += l
-    tmp = ''
-    if KeepCR == False:
+        pl = l
+
+    if not KeepCR:
+        tmp = ''
         for line in pml.split('\n'):
             if len(''.join(line.split()))>0: # only keep lines with text content
                 tmp += line + '\n'
